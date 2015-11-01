6e02598ac399c2ef38c81c18493568e3c19fa2b4
zutrinken <xoxo@peteramende.de>
2015-11-01 03:45:59 +0100

    ground zero

---
+++ README.md
@@ -0,0 +1,51 @@
+# Bleak
+
+An AJAX driven magazine styled theme for [Ghost](http://github.com/tryghost/ghost/) by [Peter Amende](http://zutrinken.com/).
+
+## Demo
+
+* [Blog](http://bleak.zutrinken.com)
+* [Post](http://bleak.zutrinken.com/demo)
+* [Tag Archive](http://bleak.zutrinken.com/tag/general)
+* [Author Archive](http://bleak.zutrinken.com/author/zutrinken)
+
+## Setup
+
+To enable [Disqus](https://disqus.com/) comments go to your blogs code injection settings and add `<script>var disqus = ‚YOUR_DISQUS_SHORTNAME‘;</script>` to your blog header.
+
+## Development
+
+Install [Grunt](http://gruntjs.com/getting-started/):
+
+	npm install -g grunt-cli
+	
+Install Grunt modules:
+
+	npm install
+
+Install [Bower](http://bower.io):
+
+	npm install -g bower
+
+Install Bower components:
+
+	bower install
+
+Build Grunt project:
+
+	grunt
+
+Distribute Grunt project:
+
+	grunt build
+
+## Copyright & License
+
+Copyright (C) 2015 Peter Amende - Released under the MIT License.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ src/js/script.js
@@ -0,0 +1,209 @@
+jQuery(function($) {
+
+	var html = $('html');
+	var body = $('body');
+
+	body.on('click', '[data-action="menu"]', function() {
+		var action = $(this).data('action');
+		var target = $('[data-target="' + $(this).data('target') + '"]').not('[data-action]');
+		menu(target)
+	});
+	
+	var menuActive = false;
+	function menu(target) {
+		if(!menuActive) {
+			html.addClass('menu-initial');
+			target.addClass('initial');
+			setTimeout(function() {
+				html.addClass('menu-active');
+				target.addClass('active');
+			}, 1);
+			menuActive = true;
+		} else {
+			target.removeClass('active');
+			html.removeClass('menu-active');
+			setTimeout(function() {
+				target.removeClass('initial');
+				html.removeClass('menu-initial');
+			}, 300);
+			menuActive = false;
+		}
+	}
+
+	body.on('click', '.overlay, #menu a', function() {
+		if (html.hasClass('menu-active')) {
+			var target = $('[data-target="menu"]').not('[data-action]');
+			menu(target);
+		}
+	});
+	
+	var body = $('body');
+	
+	/* ==========================================================================
+	   Masonry
+	   ========================================================================== */
+	
+	function grid() {
+		var postlist = $('.post-list').masonry({
+			itemSelector			: '.post',
+			isAnimated				: false,
+			gutter					: 0,
+			columnWidth				: 1,
+			transitionDuration		: 0
+		}).imagesLoaded().progress(function() {
+			postlist.masonry('layout');
+		});
+	}
+	grid();
+
+	/* ==========================================================================
+	   Run Highlight
+	   ========================================================================== */
+
+	function highlight() {
+		$('pre code').each(function(i, e) {
+			hljs.highlightBlock(e);
+			var code = $(this);
+			var lines = code.html().split(/\n/).length;
+			var numbers = [];
+			for (i = 1; i < lines; i++) {
+				numbers += '<span class="line">' + i + '</span>';
+			}
+			code.parent().addClass('codeblock').append('<div class="lines">' + numbers + '</div>');
+		});
+	}
+	highlight();
+
+	/* ==========================================================================
+	   Fitvids
+	   ========================================================================== */
+
+	function video() {
+		$('#wrapper').fitVids();
+	}
+	video();
+		
+	/* ==========================================================================
+	   Add class for ajax loading
+	   ========================================================================== */
+
+	function ajaxLinkClass() {
+		
+		$('a[href^="' + window.location.origin + '"], .post-meta a, .post-tags a, #pagination a').each(function() {
+			var link = $(this);
+			
+			if(!link.hasClass('rss')) {
+				link.addClass('js-ajax-link');
+				
+				if (link.attr('href').indexOf('page') > -1) {
+					link.addClass('js-archive-index');
+				} else {
+					link.addClass('js-show-index');
+				}
+				
+				if (link.attr('href').indexOf('tag') > -1) {
+					link.addClass('js-tag-index');
+				}
+				
+				if (link.attr('href').indexOf('author') > -1) {
+					link.addClass('js-author-index');
+				}
+			}
+		});
+	}
+	ajaxLinkClass();
+		
+	/* ==========================================================================
+	   Initialize and load Disqus
+	   ========================================================================== */
+	
+	function comments() {
+		if (typeof disqus === 'undefined') {
+			$('.post-comments').css({
+				'display' : 'none'
+			});
+		} else {
+			$.ajax({
+				type: "GET",
+				url: "//" + disqus + ".disqus.com/embed.js",
+				dataType: "script",
+				cache: true
+			});
+		}
+	}
+	comments();
+		
+	/* ==========================================================================
+	   Reload all scripts after AJAX load
+	   ========================================================================== */
+		
+	function reload() {
+		grid();
+		ajaxLinkClass();
+		highlight();
+		video();
+		comments();
+	}
+
+	/* ==========================================================================
+	   Ajax Loading
+	   ========================================================================== */
+
+	var History = window.History;
+	var loading = false;
+	var ajaxContainer = $('#ajax-container');
+	
+	if (!History.enabled) {
+		return false;
+	}
+
+	History.Adapter.bind(window, 'statechange', function() {
+		html.addClass('loading');
+		var State = History.getState();
+		$.get(State.url, function(result) {
+			var $html = $(result);
+			var newContent = $('#ajax-container', $html).contents();
+			var title = result.match(/<title>(.*?)<\/title>/)[1];
+			
+			ajaxContainer.fadeOut(500, function() {
+				document.title = title;
+				ajaxContainer.html(newContent);
+				body.removeClass();
+				body.addClass($('#body-class').attr('class'));
+				NProgress.done();
+				ajaxContainer.fadeIn(500);
+				$(document).scrollTop(0);
+				reload();
+				setTimeout(function() {
+					html.removeClass('loading');
+				}, 50);
+				loading = false;
+			});
+		});
+	});
+	$('body').on('click', '.js-ajax-link', function(e) {
+	    e.preventDefault();
+
+	    if (loading === false) {
+			var currentState = History.getState();
+			var url = $(this).prop('href');
+			var title = $(this).attr('title') || null;
+	
+	        if (url.replace(/\/$/, "") !== currentState.url.replace(/\/$/, "")) {
+				loading = true;
+				html.addClass('loading');
+				NProgress.start();
+				History.pushState({}, title, url);
+	        }
+	    }
+	});
+	
+	$('body').on('click', '.js-show-post', function() {
+		var post = $(this).parents('.post');
+		post.addClass('initial');
+		setTimeout(function() {
+			post.addClass('active');
+		}, 1);
+	});
+
+});
