409a07367ecc78729db960ba2978038f0d3994b6
zutrinken <xoxo@zutrinken.com>
2018-09-03 21:08:27 +0200

    image and gallery card support

--- src/js/script.js
+++ src/js/script.js
@@ -106,6 +106,22 @@ jQuery(function($) {
 	highlight();
 
 	/* ==========================================================================
+	   Gallery
+	   ========================================================================== */
+
+	function gallery() {
+		var images = document.querySelectorAll('.kg-gallery-image img');
+		images.forEach(function (image) {
+			var container = image.closest('.kg-gallery-image');
+			var width = image.attributes.width.value;
+			var height = image.attributes.height.value;
+			var ratio = width / height;
+			container.style.flex = ratio + ' 1 0%';
+		});
+	}
+	gallery();
+
+	/* ==========================================================================
 	   Fitvids
 	   ========================================================================== */
 
@@ -152,6 +168,7 @@ jQuery(function($) {
 		grid();
 		ajaxLinkClass();
 		highlight();
+		gallery();
 		video();
 		comments();
 		currentMenuFix();
